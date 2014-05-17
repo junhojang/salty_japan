5d780c4b415e8e3231cbdeba08fd70156c91710f
junho jang <jangjunho7@gmail.com>
2014-05-16 23:58:41 -0700

    !@@!

--- app/controllers/api/user_controller.rb
+++ app/controllers/api/user_controller.rb
@@ -2,14 +2,16 @@ class Api::UserController < ApplicationController
   
   def signup
     if params[:email].present? and params[:password].present?
-      @user = User.new
-      @user.email = params[:email]
-      @user.password = params[:password]
-      @user.password_confirmation = params[:password]
-      @user.join_method = 1
-      @user.is_active = 1
-      @user.is_admin = 0
-      @user.save
+      if !User.find_by email :params[:email] 
+        @user = User.new
+        @user.email = params[:email]
+        @user.password = params[:password]
+        @user.password_confirmation = params[:password]
+        @user.join_method = 1
+        @user.is_active = 1
+        @user.is_admin = 0
+        @user.save
+      end
     elsif params[:f_email].present?
       @user = User.new
       @user.f_email = params[:f_email]
