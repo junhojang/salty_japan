c35751988b2f328898e0615ebdbdd21845240b7a
junho jang <jangjunho7@gmail.com>
2014-05-16 23:15:29 -0700

    Com

--- app/controllers/api/user_controller.rb
+++ app/controllers/api/user_controller.rb
@@ -1,7 +1,21 @@
 class Api::UserController < ApplicationController
   
   def signup
-
+    if params[:email].present? and params[:password].present?
+      @user = User.new
+      @user.email = params[:email]
+      @user.password = params[:password]
+      @user.password_confirmnation = params[:password]
+      @user.join_method = 1
+      @user.is_active = 1
+      @user.is_admin = 0
+    elsif params[:f_email].present?
+      @user = User.new
+      @user.f_email = params[:f_email]
+      @user.join_method = 2
+      @user.is_active = 1
+      @user.is_admin = 0
+    end
   end
 
   def get_user_infos
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,5 @@
 class Client < ActiveRecord::Base
+  has_secure_password
+
   has_many :advertisement
 end
