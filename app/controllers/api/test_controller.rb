f1c915c1f639cbc3d2097dddf2957922575bd9b2
junho jang <jangjunho7@gmail.com>
2014-05-19 21:27:26 -0700

    AD

--- app/controllers/api/test_controller.rb
+++ app/controllers/api/test_controller.rb
@@ -1,8 +1,8 @@
 class Api::TestController < ApplicationController
   def get_level_test_words
     #100
-    @level  = (Level.find_by category_id: 100).first
-    @stage = (Stage.find_by level_id: @level.id).first
+    @level  = (Level.find_by category_id: 100)
+    @stage = (Stage.find_by level_id: @level.id)
     @data = Word.where(stage_id: @stage.id).limit(20)
     @status = true
     @msg = ''
